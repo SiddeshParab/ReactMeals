afdd1c0cba241bdea40e0760619b2797a861a637
Siddesh Parab <119142377+SiddeshParab@users.noreply.github.com>
2023-12-06 23:32:28 +0530

    Create Meals.jsx

---
+++ src/components/Meals/Meals.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MealsSummary from "./MealsSummary";
+import AvailableMeaks from "./AvailableMeals";
+
+const Meals = () => {
+  return (
+    <>
+      <MealsSummary />
+      <AvailableMeaks />
+    </>
+  );
+};
+
+export default Meals;
